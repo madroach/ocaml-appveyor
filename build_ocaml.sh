f1d1bbbac0549d88730bb672e8a02fc9208795b6
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-22 21:09:09 +0100

    4.09.0 -> ocaml-system

--- build_ocaml.sh
+++ build_ocaml.sh
@@ -70,7 +70,7 @@ run "opam version" opam --version
 echo "-=-=- set up OPAM -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 cd $APPVEYOR_BUILD_FOLDER
 set OPAMROOT=${OCAMLROOT}/OPAM
-opam init --yes --compiler=$OCAML_VERSION https://github.com/madroach/opam-repository.git
+opam init --yes --compiler=ocaml-system https://github.com/madroach/opam-repository.git
 # stdlib-shims 0.1 is broken on Windows
 opam pin --no-action stdlib-shims.0.2.0 "https://github.com/ocaml/stdlib-shims.git#0.2.0"
 opam install dune ocamlbuild oasis batteries containers seq iter astring cmdlinnr ounit alcotest
