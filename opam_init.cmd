509189b0fedbb3f50ab63867bb635f0627ad2778
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-26 18:44:43 +0100

    revert changes to opam_init.cmd

--- opam_init.cmd
+++ opam_init.cmd
@@ -1,28 +1,37 @@
 if [%OCAML_VERSION%]==[] set OCAML_VERSION="4.09.0"
 
-set OCAMLROOT=C:/PROGRA~1/OCaml
+set OCAMLROOT=%ProgramFiles%/OCaml
 set OPAMROOT=C:/OPAM
 
-rem set OPAM_URL="https://ci.appveyor.com/api/buildjobs/3uscc6wmf1thv0vx/artifacts/ocaml-4.09.0.zip"
+set OPAM_URL="https://ci.appveyor.com/api/buildjobs/3uscc6wmf1thv0vx/artifacts/ocaml-4.09.0.zip"
 
-rem echo Downloading opam binary %OPAM_URL%
-rem appveyor DownloadFile "%OPAM_URL%" -FileName "%temp%/ocaml.zip"
-rem cd %ProgramFiles%
-rem 7z x "%temp%/ocaml.zip"
-rem dir "%OCAMLROOT%"
-rem del "%temp%/ocaml.zip"
+echo Downloading opam binary %OPAM_URL%
+appveyor DownloadFile "%OPAM_URL%" -FileName "%temp%/ocaml.zip"
+cd %ProgramFiles%
+7z x "%temp%/ocaml.zip"
+dir "%OCAMLROOT%"
+del "%temp%/ocaml.zip"
 
-set Path=C:/cygwin/bin;%Path%
 REM Cygwin is always installed on AppVeyor.  Its path must come
 REM before the one of Git but after those of MSCV and OCaml.
+set "Path=C:/cygwin/bin;%OCAMLROOT%/bin;%OCAMLROOT%/bin/flexdll;%Path%"
 
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
-REM Cygwin is always installed on AppVeyor.  Its path must come
-REM before the one of Git but after those of MSCV and OCaml.
-set Path=%OCAMLROOT%/bin;%OCAMLROOT%/bin/flexdll;%Path%
+set CYGWINBASH=C:/cygwin/bin/bash.exe
 
-REM stdlib-shims 0.1 is broken on Windows
+if exist %CYGWINBASH% (
+  REM Make sure that "link" is the MSVC one and not the Cynwin one.
+  echo VCPATH="`cygpath -u -p '%Path%'`" > C:\cygwin\tmp\msenv
+  echo PATH="$VCPATH:$PATH" >> C:\cygwin\tmp\msenv
+  %CYGWINBASH% -lc "tr -d '\\r' </tmp/msenv > ~/.msenv64"
+  %CYGWINBASH% -lc "echo '. ~/.msenv64' >> ~/.bash_profile"
+  REM Make OCAMLROOT available in Unix form:
+  echo OCAMLROOT_WIN="`cygpath -w -s '%OCAMLROOT%'`" > C:\cygwin\tmp\env
+  (echo OCAMLROOT="`cygpath -u \"$OCAMLROOT_WIN\"`") >>C:\cygwin\tmp\env
+  echo export OCAMLROOT_WIN OCAMLROOT >>C:\cygwin\tmp\env
+  %CYGWINBASH% -lc "tr -d '\\r' </tmp/env >> ~/.bash_profile"
+)
 
 appveyor SetVariable -Name Path -Value "%Path%"
 appveyor SetVariable -Name OPAMROOT -Value "%OPAMROOT%"
