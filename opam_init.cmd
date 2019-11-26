84547d581dece5a26ac0b08ef7c610a32398fc08
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-26 12:00:45 +0100

    Revert "try last working"
    This reverts commit 1da3f64b4cd5ad9c49f4de54674812b45ecc4b1d.

--- opam_init.cmd
+++ opam_init.cmd
@@ -1,22 +1,22 @@
-set OCAMLROOT=C:/PROGRA~1/OCaml
+if [%OCAML_VERSION%]==[] set OCAML_VERSION="4.09.0"
+
+set OCAMLROOT=%ProgramFiles%/OCaml
 set OPAMROOT=C:/OPAM
 
-if not defined OCAML_BRANCH (set OCAML_BRANCH=4.09)
-set OCAMLURL="https://ci.appveyor.com/api/projects/madroach/ocaml-appveyor/artifacts/ocaml-%OCAML_BRANCH%.zip"
+set OPAM_URL="https://ci.appveyor.com/api/buildjobs/3uscc6wmf1thv0vx/artifacts/ocaml-4.09.0.zip"
 
-echo Downloading OCaml %OCAML_BRANCH% from "%OCAMLURL%"
-appveyor DownloadFile "%OCAMLURL%" -FileName "%temp%/ocaml.zip"
-cd "%PROGRAMFILES%"
-7z x -y "%temp%/ocaml.zip"
-del %temp%/ocaml.zip
-
-set Path=C:/cygwin/bin;%Path%
-
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+echo Downloading opam binary %OPAM_URL%
+appveyor DownloadFile "%OPAM_URL%" -FileName "%temp%/ocaml.zip"
+cd %ProgramFiles%
+7z x "%temp%/ocaml.zip"
+dir "%OCAMLROOT%"
+del "%temp%/ocaml.zip"
 
 REM Cygwin is always installed on AppVeyor.  Its path must come
 REM before the one of Git but after those of MSCV and OCaml.
-set Path=%OCAMLROOT%/bin;%OCAMLROOT%/bin/flexdll;%Path%
+set "Path=C:/cygwin/bin;%OCAMLROOT%/bin;%OCAMLROOT%/bin/flexdll;%Path%"
+
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
 set CYGWINBASH=C:/cygwin/bin/bash.exe
 
@@ -33,23 +33,19 @@ if exist %CYGWINBASH% (
   %CYGWINBASH% -lc "tr -d '\\r' </tmp/env >> ~/.bash_profile"
 )
 
-opam init --yes --compiler=ocaml-system https://github.com/madroach/opam-repository.git
-REM stdlib-shims 0.1 is broken on Windows
-opam pin --no-action stdlib-shims.0.2.0 "https://github.com/ocaml/stdlib-shims.git#0.2.0"
-
 appveyor SetVariable -Name Path -Value "%Path%"
 appveyor SetVariable -Name OPAMROOT -Value "%OPAMROOT%"
-set OPAM_SWITCH_PREFIX=%OPAMROOT%/ocaml-system
+set "OPAM_SWITCH_PREFIX=%OPAMROOT%/ocaml-system"
 appveyor SetVariable -Name OPAM_SWITCH_PREFIX -Value "%OPAM_SWITCH_PREFIX%"
-set OCAMLLIB=%OCAMLROOT%/lib/ocaml
+set "OCAMLLIB=%OCAMLROOT%/lib/ocaml"
 appveyor SetVariable -Name CAML_LD_LIBRARY_PATH -Value "%CAML_LD_LIBRARY_PATH%"
-set CAML_LD_LIBRARY_PATH=%OCAMLROOT%/lib/stublibs:%OPAM_SWITCH_PREFIX%/lib/stublibs
+set "CAML_LD_LIBRARY_PATH=%OCAMLROOT%/lib/stublibs:%OPAM_SWITCH_PREFIX%/lib/stublibs"
 appveyor SetVariable -Name CAML_LD_LIBRARY_PATH -Value "%CAML_LD_LIBRARY_PATH%"
-set OCAMLTOP_INCLUDE_PATH="%OPAM_SWITCH_PREFIX%/lib/toplevel"
+set "OCAMLTOP_INCLUDE_PATH=%OPAM_SWITCH_PREFIX%/lib/toplevel"
 appveyor SetVariable -Name OCAMLTOP_INCLUDE_PATH -Value "%OCAMLTOP_INCLUDE_PATH%"
-set OCAMLPATH="%OPAM_SWITCH_PREFIX%/lib
+set "OCAMLPATH="%OPAM_SWITCH_PREFIX%/lib"
 appveyor SetVariable -Name OCAMLPATH -Value "%OCAMLPATH%"
-set OCAMLRUNPARAM="bs=8M"
+set "OCAMLRUNPARAM=bs=8M"
 appveyor SetVariable -Name OCAMLRUNPARAM -Value "%OCAMLRUNPARAM%"
 
 set <NUL /p=Ready to use OCaml & ocamlc -version
