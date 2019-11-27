ca64e3d320fc0289a4f6b5cd2d9bc9ae6a9f56b7
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-27 10:17:25 +0100

    bash: del->rm

--- build_ocaml.sh
+++ build_ocaml.sh
@@ -70,7 +70,7 @@ run "opam version" opam --version
 cd ${OCAMLROOT}
 7z a "${APPVEYOR_BUILD_FOLDER}/ocaml-${OCAML_VERSION}.zip" *
 appveyor PushArtifact "${APPVEYOR_BUILD_FOLDER}/ocaml-${OCAML_VERSION}.zip"
-del "${APPVEYOR_BUILD_FOLDER}/ocaml-${OCAML_VERSION}.zip"
+rm "${APPVEYOR_BUILD_FOLDER}/ocaml-${OCAML_VERSION}.zip"
 
 echo "-=-=- set up OPAM -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 cd $APPVEYOR_BUILD_FOLDER
@@ -92,4 +92,4 @@ opam install -v --yes alcotest
 cd $OPAMROOT
 7z a "${APPVEYOR_BUILD_FOLDER}/opam-${OCAML_VERSION}.zip" *
 appveyor PushArtifact "${APPVEYOR_BUILD_FOLDER}/opam-${OCAML_VERSION}.zip"
-del "${APPVEYOR_BUILD_FOLDER}/opam-${OCAML_VERSION}.zip"
+rm "${APPVEYOR_BUILD_FOLDER}/opam-${OCAML_VERSION}.zip"
